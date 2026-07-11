211a70abd6c060c7855ff1762944d336153441d6
Hoodwise <hoodwise50@gmail.com>
2026-07-11 21:07:54 +0700

    Add SEO knowledge hub foundation

--- src/app.js
+++ src/app.js
@@ -69,6 +69,7 @@ function createApp() {
   app.get('/favicon.ico', (req, res) => {
     res.type('image/svg+xml').sendFile(path.join(publicDir, 'hoodwise-mark.svg'));
   });
+  app.get('/guides', (req, res) => res.sendFile(path.join(publicDir, 'guides.html')));
   app.get('/ops', (req, res) => res.sendFile(path.join(publicDir, 'ops.html')));
   app.get(['/app', '/app/', '/app/c/:conversationId'], (req, res) => {
     res.sendFile(path.join(publicDir, 'app.html'));
